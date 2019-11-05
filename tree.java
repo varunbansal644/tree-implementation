f493deaba256f7d0e6bf635e14ab84566ac2fcaf
varunbansal644 <varunbansal644@gmail.com>
2019-11-05 10:27:48 +0530

    Basic Node Class

---
+++ tree.java
@@ -0,0 +1,10 @@
+public class Tree{
+    class Node{
+        int data;
+        Node left,right;
+        Node(int value){
+            this.data=value;
+            left=right=null;
+        }
+    }
+}
